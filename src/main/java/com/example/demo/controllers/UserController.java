7d4bfa0ee22338c95c73e6c1909d1bacb2ea6783
Isidora Havlovic <125749938+havlisi@users.noreply.github.com>
2023-09-11 13:29:42 +0200

    Added security

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,19 +1,29 @@
 package com.example.demo.controllers;
 
+import java.sql.Date;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.entities.UserEntity;
 import com.example.demo.entities.dto.UserEntityDTO;
+import com.example.demo.entities.dto.UserTokenDTO;
 import com.example.demo.repositories.UserRepository;
+import com.example.demo.util.Encryption;
+
+import io.jsonwebtoken.Jwts;
 
 @RestController
 @RequestMapping(path = "api/v1/user")
@@ -21,6 +31,13 @@ public class UserController {
 	
 	@Autowired
 	UserRepository userRepository;
+
+	@Autowired
+	private SecretKey secretKey;
+	
+	@Value("${spring.security.token-duration}")
+	private Integer tokenDuration;
+	
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<?> getAll() {
@@ -30,7 +47,7 @@ public class UserController {
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
 	public ResponseEntity<?> getById(@PathVariable Integer id) {
-		UserEntity user = userRepository.findById(id);
+		Optional<UserEntity> user = userRepository.findById(id);
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 	
@@ -40,13 +57,45 @@ public class UserController {
 		
 		user.setFirstName(newUser.getFirstName());
 		user.setLastName(newUser.getLastName());
+		user.setUsername(newUser.getUsername());
 		user.setEmail(newUser.getEmail());
 		user.setRole("ROLE_USER");
-		user.setPassword(newUser.getFirstName());
+		
+		if (newUser.getConfirmed_password() == newUser.getPassword()) {
+			user.setPassword(newUser.getFirstName());
+		}
+		
 		user.setFirstName(newUser.getFirstName());
+		userRepository.save(user);
 		
+		return new ResponseEntity<UserEntityDTO>(new UserEntityDTO(user), HttpStatus.CREATED);
+	}
+	
+	private String getJWTToken(UserEntity userEntity) {
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList(userEntity.getRole());
+		String token = Jwts.builder().setId("softtekJWT").setSubject(userEntity.getEmail())
+				.claim("authorities",
+						grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + this.tokenDuration)).signWith(this.secretKey)
+				.compact();
+		return "Bearer " + token;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, path = "/login")
+	public ResponseEntity<?> login(@RequestBody Map<String, String> korisnikLogger) {
+		String email = korisnikLogger.get("email");
+		String password = korisnikLogger.get("password");
+		UserEntity user = userRepository.findByEmail(email);
+		if (user != null && Encryption.validatePassword(password, user.getPassword())) {
+			String token = getJWTToken(user);
+			UserTokenDTO userLogin = new UserTokenDTO(user.getId(), email, token, user.getRole());
+			
+			return new ResponseEntity<>(userLogin, HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Wrong credentials", HttpStatus.UNAUTHORIZED);
 		
-		return new ResponseEntity<>(user, HttpStatus.CREATED);
 	}
 	
 	
